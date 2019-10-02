0203ec33fffc9af0286e1ac63d7a99530e41a2eb
lagagain <lagagain@email.com>
2019-10-02 21:26:53 +0800

    Add Blog Post Imgration

---
+++ database/migrations/2019_10_02_182402_create_blog_post_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_post', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->lineString("titile")->comment('標題');
+            $table->longText("content")->comment('文章內容');
+            //$table->string('author')->comment("作者");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_post');
+    }
+}
